8cefa93377524c6f9c0d9c3f9eb9bc7ff0809555
NguyenTran <dm2019.ttnguyen.3105@gmail.com>
2021-06-08 21:26:33 +0700

    set up source code

---
+++ backend/models/Product.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = new mongoose.Schema({
+    _id:{
+        type:'string',
+        require: true
+    },
+    title:{
+        type: 'string',
+        require: true
+    },
+    src:{
+        type:'array',
+        require: true
+    },
+    description:{
+        type: 'string',
+        require: true
+    },
+    content:{
+        type: 'string',
+        require: true
+    },
+    price:{
+        type: 'number',
+        require: true
+    },
+    kind:{
+        type: 'string',
+        require: true
+    },
+    size:{
+        type: 'array',
+        require: true
+    },
+    count:{
+        type: 'number',
+        require: true
+    },
+    quantities:{
+        type: 'number',
+        require: true,
+    }
+})
+
+const Product = mongoose.model('Product',productSchema)
+
+module.exports=Product;
