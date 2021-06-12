ea7978f6202b7230d69c055a7094455cb1511ec3
Kimnguyen035 <kimnguyen035171@gmail.com>
2021-06-12 22:16:08 +0700

    code order

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 connectDB();
 
@@ -10,8 +11,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/products',productRouter);
-app.use('/api',userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/user', userRouter);
+app.use('/api/order', orderRouter);
 
 const PORT = process.env.PORT || 5000;
 
