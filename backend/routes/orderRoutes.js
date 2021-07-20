1568242c222f48fb2e267260e6613c1269c033c8
Kimnguyen035 <kimnguyen035171@gmail.com>
2021-07-20 21:07:10 +0700

    api order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-const {orders_create_order,
-    orders_get_all,
-    orders_get_order,
-    orders_delete_order} = require( '../controller/orderController');
+const {
+    getSumary,
+    getOrderById,
+    createOrder
+    // orders_create_order,
+    // orders_get_all,
+    // orders_get_order,
+    // orders_delete_order
+} = require( '../controller/orderController');
     
-router.get('/', orders_get_all);
+// router.get('/', orders_get_all);
 
-router.post('/', orders_create_order);
+// router.post('/', orders_create_order);
 
-router.get('/:orderId', orders_get_order);
+// router.get('/:orderId', orders_get_order);
 
-router.delete('/:orderId', orders_delete_order);
+// router.delete('/:orderId', orders_delete_order);
+
+router.get('/sumary', getSumary);
+
+router.get('/:id', getOrderById);
+
+router.post('/', createOrder);
 
 module.exports = router;
